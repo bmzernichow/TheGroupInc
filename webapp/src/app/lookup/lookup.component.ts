789b162923e8c3196f157b4f62da07e4e7266c0c
BMariusZernichow <bm.zernichow@outlook.com>
2016-11-11 13:59:15 +0100

    Http fetch update

--- webapp/src/app/lookup/lookup.component.ts
+++ webapp/src/app/lookup/lookup.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {Http, Response, Headers} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
@@ -8,55 +8,26 @@ import 'rxjs/add/operator/map';
   templateUrl: './lookup.component.html',
   styleUrls: ['./lookup.component.css']
 })
+
 export class LookupComponent {
 
-  superhero: string = null;
   value: string = '';
-  address = '';
-  hero: string = null;
-
-  superheroes = ['Hulk', 'Flash', 'Superman', 'Batman', 'Spiderman', 'Iron Man', 'Thor', 'Wolverine', 'Deadpool'];
-  superheroeines = ['Catwoman', 'She-Hulk', 'Wonder Woman', 'Batwoman', 'Invisible Woman'];
-
-  scopes = [
-    { value: 'All', icon: 'groups' },
-    { value: 'Men', icon: 'user' },
-    { value: 'Women', icon: 'lead' },
-  ];
-
-  scope = this.scopes[0];
+  orgUnit = '';
 
   constructor(private http: Http) {}
 
-  lookup = (query: string, source = this.superheroes): string[] => {
-    if (!query) {
-      return null;
-    }
-
-    return source.filter((d: string) => d.toLowerCase().indexOf(query.toLowerCase()) > -1);
-  }
-
-  // This function is now safe to pass around
+  // Lookup async function for organisation units
   lookupAsync = (query: string): Observable<any[]> => {
     if (!query) {
       return null;
     }
 
-    return this.http.get(`//maps.googleapis.com/maps/api/geocode/json?address=${query}`)
+    var headers = new Headers();
+    headers.append('Authorization', "Basic " + btoa("admin:district"));
+
+    return this.http.get(`https://play.dhis2.org/test/api/organisationUnits.json?paging=false&filter=displayName:ilike:${query}`, {headers: headers})
       .map((res: Response) => res.json())
-      .map((response: any) => response.results);
-  }
-
-  lookupPolymorphic = (query: string): string[] => {
-    let heroes: string[];
-    if (this.scope.value === 'Men') {
-      heroes = [ ...this.superheroes ];
-    } else if (this.scope.value === 'Women') {
-      heroes = [ ...this.superheroeines ];
-    } else {
-      heroes = [ ...this.superheroes, ...this.superheroeines ];
-    }
-
-    return this.lookup(query, heroes);
+      .map((response: any) => response.organisationUnits);
+    
   }
 }
