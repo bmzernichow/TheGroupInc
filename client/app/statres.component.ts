d9d44238908aaaa3f15ef566a3f1d3a8869dc00a
jorgebje <jorgebje@ifi.uio.no>
2016-11-11 16:02:07 +0100

    removed client from master

--- client/app/statres.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-	moduleId: module.id,
-  selector: 'my-statres',
-  //template: '<h2>My StatRes in Angular</h2>',
-  templateUrl: 'statres.component.html',
-})
-export class StatResComponent { }
