27b2ae5eb91456f32bac587dfcf6e53577860920
jorgebje <jorgebje@ifi.uio.no>
2016-11-15 14:10:45 +0100

    added bar chart

--- webapp/src/app/chart/chart.component.ts
+++ webapp/src/app/chart/chart.component.ts
@@ -10,10 +10,23 @@ import {SharedService} from '../shared.service';
 export class ChartComponent {
 
   data: any;
+  data2: any;
   options: any;
+  options2: any;
 
   constructor(private sharedService: SharedService) {
     this.data = {
+      labels: [],
+      datasets: [
+        {
+          label: 'DHIS2 chart',
+          data: [],
+          fill: true,
+          borderColor: '#4bc0c0'
+        }
+      ]
+    }
+    this.data2 = {
       labels: [],
       datasets: [
         {
@@ -31,23 +44,43 @@ export class ChartComponent {
         position: 'top'
       }
     };
+    this.options2 = {
+      responsive: true,
+      legend: {
+        position: 'top'
+      }
+    };
   }
 
-  // get date and count values from shared.service.ts
-  getChartData() {
-    let _date = this.sharedService.getChartLabels(this.sharedService.data2, 'date');
-    let _values = this.sharedService.getChartLabels(this.sharedService.data2, 'value');
-    this.data = {
+  getData(_date, _values,borderColor){
+    return  {
       labels: _date,
       datasets: [
         {
           label: 'DHIS2 chart',
           data: _values,
           fill: true,
-          borderColor: '#4bc0c0'
+          // backgroundColor: '',
+          borderColor: borderColor //'#4bc0c0'
         }
       ]
     }
+  }
+
+  // get date and count values from shared.service.ts
+  getChartData() {
+    let _date = this.sharedService.getChartLabels(this.sharedService.data2, 'date');
+    let _values = this.sharedService.getChartLabels(this.sharedService.data2, 'value');
+    // this.data = this.getData();
+    this.data = this.getData(_date,_values,'#00F');
+    this.getChartData2();
+  }
+
+  getChartData2(){
+    let _date = this.sharedService.getChartLabels(this.sharedService.data2, 'date');
+    let _values = this.sharedService.getChartLabels(this.sharedService.data2, 'value');
+    // this.data = this.getData();
+    this.data2 = this.getData(_date,_values,'#00F');
   }
 
 }
