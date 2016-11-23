9effc2d42bf6d3ce3a7a874cfcfba5ace8147a8c
BMariusZernichow <bm.zernichow@outlook.com>
2016-11-23 11:40:38 +0100

    Update

--- src/app/lookup/lookup.component.ts
+++ src/app/lookup/lookup.component.ts
@@ -42,18 +42,8 @@ export class LookupComponent {
   ];
   // items array for the periodMovingAverage menu
   items2 = [
-    {value: '1'},
-    {value: '2'},
     {value: '3'},
-    {value: '4'},
-    {value: '5'},
-    {value: '6'},
-    {value: '7'},
-    {value: '8'},
-    {value: '9'},
-    {value: '10'},
-    {value: '11'},
-    {value: '12'},
+    {value: '5'}
   ];
 
   // function for generating url from unit, indicator and period and returning http response
